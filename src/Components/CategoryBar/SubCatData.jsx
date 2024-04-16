0d9b0c66d454ce58cf671afe90dacdb42819f4e6
munkeyyy <r2256308@gmail.com>
2024-04-16 16:07:45 +0530

    add subcat list from backend

--- src/Components/CategoryBar/SubCatData.jsx
+++ src/Components/CategoryBar/SubCatData.jsx
@@ -6,21 +6,26 @@ const SubCatData = () => {
     const[subCat, setSubCat]=useState([])
 
     useEffect(()=>{
-        axios.get(baseUrl+"sub-categories/get-sub-categories")
-        .then((res)=>setSubCat(res.data.data))
+        axios.get(baseUrl+"categories/get-categories")
+        .then((res)=>{setSubCat(res.data.data)
+             console.log(res.data.data)})
         .catch((err)=>console.log(err))
     },[])
   return (
-    <div className='grid bg-white grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-10'>
+    <div className='grid bg-white grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mb-4'>
         {
-            subCat.map((elem, i)=>(
-                <div key={i} className=''>
-                    <h1 className='roboto text-[.9vw] text-black font-semibold'>{elem?.category.title}</h1>
-                    <ul className='mt-3'>
-                        <li className='text-black cursor-pointer font-normal text-[.9vw] transition-[all1s] hover:text-[#00a49f]'>{elem?.title}</li>
-                    </ul>
-                </div>
-            ))
+            subCat.map((elem,i)=>{
+                return (
+                    <div key={i} className='h-[max-content]'>
+                        <h1 className='font-medium text-[1vw] roboto'>{elem.title}</h1>
+                        {elem.subcategory.map((sub,i)=>(
+                            <ul>
+                                <li className='roboto' key={i}>{sub.title}</li>
+                            </ul>
+                        ))}
+                    </div>
+                )
+            })
         }
 
     </div>
