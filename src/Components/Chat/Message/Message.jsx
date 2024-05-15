487fce1dec0401f20471862755b8ad0f7928d5ad
munkeyyy <r2256308@gmail.com>
2024-05-15 09:27:20 +0530

    add new date-time format

--- src/Components/Chat/Message/Message.jsx
+++ src/Components/Chat/Message/Message.jsx
@@ -5,35 +5,39 @@ import { LoginContext } from "../../../contexts/Login/LoginContext";
 import { UserContext } from "../../../contexts/User/UserContext";
 import moment from "moment";
 
-const Message = ({message}) => {
-  const{selectedConversation}=useContext(ConversationContext)
-  const{user}=useContext(UserContext)
-  const LoggedInUser=message?.senderId===user._id
-  const className = LoggedInUser? "chat-end" :"chat-start"
-  const profilePic =LoggedInUser? user?.avatar :selectedConversation?.avatar
-  const bubbleColor= LoggedInUser?"bg-[#ceddff]" : "bg-gray-300"
-  console.log("receive",message.receiverId)
-  console.log("Send", message.senderId)
+const Message = ({ message }) => {
+  const { selectedConversation } = useContext(ConversationContext);
+  const { user } = useContext(UserContext);
+  const LoggedInUser = message?.senderId === user._id;
+  const className = LoggedInUser ? "chat-end" : "chat-start";
+  const profilePic = LoggedInUser ? user?.avatar : selectedConversation?.avatar;
+  const bubbleColor = LoggedInUser ? "bg-[#ceddff]" : "bg-gray-300";
+  console.log("receive", message.receiverId);
+  console.log("Send", message.senderId);
   return (
     <>
-    <div className={`chat mt-4 ${className}`}>
-      <div className="chat-image avatar"> 
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src={profilePic?`
-            https://olx-backend-pexw.onrender.com/uploads/users/${profilePic}`:Avatar}
-          />
+      <div className={`chat mt-4 ${className}`}>
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src={
+                profilePic
+                  ? `
+            https://olx-backend-pexw.onrender.com/uploads/users/${profilePic}`
+                  : Avatar
+              }
+            />
+          </div>
+        </div>
+        <div className={`chat-bubble ${bubbleColor} text-black`}>
+          {message.message}.
+        </div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1  items-center">
+          {moment(message.createdAt).format("LT")}
         </div>
       </div>
-      <div className={`chat-bubble ${bubbleColor} text-black`}>
-        {message.message}.
-      </div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1  items-center">
-     
-      </div>
-    </div>
-    {/* <div className="chat chat-end mt-4">
+      {/* <div className="chat chat-end mt-4">
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
           <img
